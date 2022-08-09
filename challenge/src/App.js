68077bf76c69e60029b0ba4fca4295f4db8636ab
Frank Benitez <frank2012cu@gmail.com>
2022-08-08 22:18:31 -0400

    barra gris casi completada

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/header/Navbar.js';
+import SubNavbar from './components/header/SubNavbar';
+
+
 function App() {
   return (
     <div >
      <Navbar/>
+     <SubNavbar/>
     </div>
   );
 }
